762a988fe7488d0898bb9ce1edb722cd4256be6e
Lowe Löwing <lowe.lowing@gmail.com>
2024-11-04 11:41:34 +0100

    first commit

---
+++ src/routes/Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let input = $state('');
+	let html = $state('');
+
+	const handleSearch = async () => {
+		const res = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) =>
+			response.json()
+		);
+		html = JSON.stringify(res);
+	};
+</script>
+
+<div class="input">
+	<input type="text" bind:value={input} />
+	<button onclick={handleSearch}>search</button>
+</div>
+{html}
+
+<style>
+	.input {
+		margin-bottom: 20px;
+	}
+</style>
